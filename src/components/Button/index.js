9e8f1411596a907e6e0e4086cf1d6ec21285742b
Piotr Sitarek <piotr.sitarek.05@gmail.com>
2020-03-16 11:53:19 +0100

    First edit

---
+++ src/components/Button/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./style.scss";
+
+
+const Button = () => {
+
+    return (
+        <button>Click me</button>
+    )
+}
+
+
+
+export default Button;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "../../components/Button"
+import "./style.scss";
+
+
+const Home = () => {
+
+    return (
+        <>
+            <h1>Home</h1>
+            <Button />
+            <br></br>
+            <Button />
+        </>
+    )
+}
+
+
+
+export default Home;
